12353a9a5f32157ae14acdd3ec237010843153be
Leonid <neustroev.leonid@mail.ru>
2020-11-18 23:04:26 +0300

    Ex.11-12

---
+++ src/main11_12.java
@@ -0,0 +1,10 @@
+public class main11_12  {
+    public static void main(String[] args) {
+       SampleHelloWorld helloWorld= new SampleHelloWorld();
+        helloWorld.launcchthis();
+        /*Calculator calculator = new Calculator();
+        calculator.thisStart();*/
+    }
+
+}
+
